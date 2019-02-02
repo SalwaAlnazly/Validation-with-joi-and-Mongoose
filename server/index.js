197a65d707a16391d9e917286a165411bbf213cc
salwaAlnazly <herotony111@gmail.com>
2019-02-02 11:26:59 +0200

    Catch 404 Errors and forward them to error handler

--- server/index.js
+++ server/index.js
@@ -2,15 +2,21 @@ const app = require('express')()
 const logger = require('morgan')
 
 
-// Middlewares
-app.use(logger())
+// Middleware
+app.use(logger('dev'))
 
 // Routes
 
 // Catch 404 Errors and forward them to error handler
-
+app.use((req, res, next) => {
+    const err = new Error('Not found')
+    err.status = 404
+    next(err)
+})
 
 // Error handler function
+
 // start the server
 const port = app.get('port') || 8080
 app.listen(port, () => console.log(`Server is listening on port ${port}`));
+ 
