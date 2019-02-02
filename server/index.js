ccd8a2ee972b5a28c185810fb29eae8036660186
salwaAlnazly <herotony111@gmail.com>
2019-02-02 11:20:49 +0200

    install morgan and use it as middleware

--- server/index.js
+++ server/index.js
@@ -1,5 +1,16 @@
 const app = require('express')()
+const logger = require('morgan')
 
+
+// Middlewares
+app.use(logger())
+
+// Routes
+
+// Catch 404 Errors and forward them to error handler
+
+
+// Error handler function
 // start the server
 const port = app.get('port') || 8080
 app.listen(port, () => console.log(`Server is listening on port ${port}`));
