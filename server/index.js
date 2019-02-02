dbd30d680fca910a229dd3af687ecad7f9463871
salwaAlnazly <herotony111@gmail.com>
2019-02-02 11:41:33 +0200

    Error handler function

--- server/index.js
+++ server/index.js
@@ -6,6 +6,11 @@ const logger = require('morgan')
 app.use(logger('dev'))
 
 // Routes
+app.get('/', (req, res, next) => {
+    res.status(200).json({
+        message: "You requested index page"
+    })
+})
 
 // Catch 404 Errors and forward them to error handler
 app.use((req, res, next) => {
@@ -15,7 +20,21 @@ app.use((req, res, next) => {
 })
 
 // Error handler function
+app.use((err, req, res, next) => {
+    const error = app.get('env') === 'development' ? err : {}
+    const status = err.status || 500
 
+    //Respond to client
+    res.status(status).json({
+        error: {
+            message: error.message
+        }
+    })
+     
+    // Respond to ourselves
+    console.error(err);
+    
+})
 // start the server
 const port = app.get('port') || 8080
 app.listen(port, () => console.log(`Server is listening on port ${port}`));
